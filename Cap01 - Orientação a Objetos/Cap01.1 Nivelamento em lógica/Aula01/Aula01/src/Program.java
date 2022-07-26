a34cbd2e31cd7bc98db4157862a9198820573d4b
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2022-07-25 22:04:58 -0300

    Aula01.

---
+++ Cap01 - Orientação a Objetos/Cap01.1 Nivelamento em lógica/Aula01/Aula01/src/Program.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        int n1, n2;
+        float f1, f2;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.print("Digite o valor de n1: ");
+        n1 = leitura.nextInt();
+        System.out.print("Digite o valor de n2: ");
+        n2 = leitura.nextInt();
+
+        System.out.print("Digite o valor de f1: ");
+        f1 = leitura.nextFloat();
+        System.out.print("Digite o valor de f2: ");
+        f2 = leitura.nextFloat();
+
+        // Adição
+
+        // Subtração
+
+        // Multiplicação
+
+        // Divisão
+
+        leitura.close();
+
+        // As quatro operações básicas com os tipos int e float
+
+    }
+
+}
